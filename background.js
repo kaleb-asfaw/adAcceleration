ef5099b34460943316d8eb8fc3034bb11a20ec98
Kaleb Asfaw <122475977+kaleb-asfaw@users.noreply.github.com>
2024-02-09 13:13:07 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ "totalTimeSaved": 0, "adSkipperEnabled": true });
+    chrome.storage.local.set({ "totalTimeSaved": 0, "timeSavedSinceReset": 0, "lastResetDate": new Date().toISOString(), "adSkipperEnabled": true });
+    // initialize id in superbase upon installation HERE
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "resetTimeSaved") {
+        chrome.storage.local.set({ "timeSavedSinceReset": 0, "lastResetDate": new Date().toISOString() });
+    }
+    
     if (request.action === "updateTimeSaved") {
-        chrome.storage.local.get("totalTimeSaved", (result) => {
+        chrome.storage.local.get(["totalTimeSaved", "timeSavedSinceReset"], (result) => {
             let totalTimeSaved = result.totalTimeSaved || 0;
-            totalTimeSaved += request.timeSaved;
-            chrome.storage.local.set({ "totalTimeSaved": totalTimeSaved });
+            let timeSavedSinceReset = result.timeSavedSinceReset || 0;
+
+            totalTimeSaved += request.timeSaved; // Value that is pushed to database
+
+            // UPDATE TOTALTIMESAVED IN DB
+
+            timeSavedSinceReset += request.timeSaved;
+            chrome.storage.local.set({ "totalTimeSaved": totalTimeSaved, "timeSavedSinceReset": timeSavedSinceReset });
         });
     }
 });
